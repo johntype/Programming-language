28db6de683b24bfa1b17070968c6a5c1fa202ecd
Zhenya <zhenya@mail.ru>
2022-07-25 13:35:13 +0900

    add Task_10

---
+++ Seminar/Seminar2/HomeWork/Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = removeFirstThird(num);
+
+Console.WriteLine($"{num} -> {res}");
+
+int removeFirstThird(int num)
+{
+    int first = num / 100;
+    int two = num / 10 % 10;
+    int third = num % 10;
+    return two;
+}
+
