ea364ef1403abfa3a96683f5da31e8ef692a68c6
Zhenya <zhenya@mail.ru>
2022-07-25 13:35:33 +0900

    add Task_15

---
+++ Seminar/Seminar2/HomeWork/Task_15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int day_week = Convert.ToInt32(Console.ReadLine());
+
+if (day_week > 7 || day_week < 1)
+{
+    Console.WriteLine("Ошибка");
+}
+else if (day_week == 6 || day_week == 7)
+{
+    Console.WriteLine($"{day_week} -> Да");
+}
+else
+{
+    Console.WriteLine($"{day_week} -> Нет");
+}
