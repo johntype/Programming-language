004230aca9690d836caa9fe64158e8fad54ff9ce
Zhenya <zhenya@mail.ru>
2022-08-07 16:23:54 +0900

    add Task_27

---
+++ Seminar/Seminar4/HomeWork/Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int result = GetSumNumber(num);
+Console.WriteLine($"{num} -> {result}");
