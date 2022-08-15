4c18188c31cf2b53932cd71de10695c7cfba3cb4
Zhenya <zhenya@mail.ru>
2022-08-15 16:36:12 +0900

    add Task_49

---
+++ Seminar/Seminar7/Task_49/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] SqareEvenMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                matrix[i, j] *= matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],2} ,");
+            else Console.Write($"{array[i, j],2}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResult = GetMatrix(3, 4);
+PrintMatrix(matrixResult);
+Console.WriteLine();
+int[,] newMatrix = SqareEvenMatrix(matrixResult);
+PrintMatrix(newMatrix);
