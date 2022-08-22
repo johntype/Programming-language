854fca7acab3671f72f3fc46bd5e44cbb0cb1abb
Zhenya <zhenya@mail.ru>
2022-08-22 23:29:09 +0900

    add Task_53

---
+++ Seminar/Seminar8/Task_53/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],2} ,");
+            else Console.Write($"{array[i, j],2}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+    }
+}
+
+int[,] matrix = GetMatrix(3, 3);
+PrintMatrix(matrix);
+SortArray(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
