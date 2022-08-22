ac9d73b019ee50e38de44efff0bfd65dc8167f04
Zhenya <zhenya@mail.ru>
2022-08-22 23:46:13 +0900

    add Task_57

---
+++ Seminar/Seminar8/Task_57/Program.cs
@@ -0,0 +1,143 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+// Набор данных Частотный массив
+// { 1, 9, 9, 0, 2, 8, 0, 9 } 
+// 0 встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+string[] GetUserInputNumbersString(string userInputTextString)
+{
+    Console.WriteLine(userInputTextString);
+    string[] userInputString = Console.ReadLine().Split(",");
+    return userInputString;
+}
+
+void CheckUserInputToInt(string[] userInputString)
+{
+    for (int i = 0; i < userInputString.Length; i++)
+    {
+        if (userInputString[i] == string.Empty || userInputString[i] == " "
+            || Convert.ToInt32(userInputString[i]) == null
+            || userInputString.Length < 2)
+        {
+            Console.WriteLine("Ошибка ввода данных. Попробуйте еще раз запустить программу и ввести данные корректно.");
+            Environment.Exit(0);
+        }
+    }
+}
+
+int[] ConvertUserInputNumbersInt(string[] userNumberString)
+{
+    int[] userNumberInt = new int[userNumberString.Length];
+    for (int i = 0; i < userNumberString.Length; i++)
+    {
+        userNumberInt[i] = Convert.ToInt32(userNumberString[i]);
+    }
+    return userNumberInt;
+}
+
+void FillMatrix2DInt(int[,] matrixTwoDimensional,
+                                int minIntervalValue, int maxIntervalValue)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            matrixTwoDimensional[i, j] = random.Next(minIntervalValue, maxIntervalValue);
+        }
+    }
+}
+
+int[] ConversionArray(int[,] matrix)
+{
+    int[] arraySort = new int[matrix.Length];
+    int m = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arraySort[m] = matrix[i, j];
+            m++;
+        }
+    }
+    Array.Sort(arraySort);
+    return arraySort;
+}
+
+void OutputCountNumbersToConsole(int[] array)
+{
+    int number = array[0];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number) count++;
+        else
+        {
+            Console.WriteLine($"Число {number} встречается --> {count} раз");
+            number = array[i];
+            count = 1;
+        }
+        if (i == array.Length - 1)
+            Console.WriteLine($"Число {number} встречается --> {count} раз");
+    }
+}
+
+void PrintArray(int[] array, string preOutputText, string postOutputText)
+{
+    Console.Write(preOutputText);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}] ");
+    }
+    Console.WriteLine(postOutputText);
+}
+
+void PrintMatrix2DInt(int[,] matrixTwoDimensional, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Clear();
+string[] matrixSizeString = GetUserInputNumbersString("Введите количество строк и столбцов массива через запятую: ");
+CheckUserInputToInt(matrixSizeString);
+int[] matrixSizeInt = ConvertUserInputNumbersInt(matrixSizeString);
+string[] matrixMinMaxString = GetUserInputNumbersString("Введите границы интервала случайных чисел (через запятую): ");
+CheckUserInputToInt(matrixMinMaxString);
+int[] matrixMinMaxInt = ConvertUserInputNumbersInt(matrixMinMaxString);
+int[,] matrix = new int[matrixSizeInt[0], matrixSizeInt[1]];
+FillMatrix2DInt(matrix, matrixMinMaxInt[0], matrixMinMaxInt[1]);
+PrintMatrix2DInt(matrix, "Исходный масси: ");
+int[] array = ConversionArray(matrix);
+PrintArray(array, "Массив переведенный в строку: ", String.Empty);
+OutputCountNumbersToConsole(array);
